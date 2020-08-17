5c13cbfc6a3f997dd62ef212063343e019d5798e
MahiPalihawadana <dilkipalihawadana95@gmail.com>
2020-08-17 22:32:52 +0530

    version 1 added

---
+++ src/app/rfs/rfs.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { FormControl , FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-rfs',
+  templateUrl: './rfs.component.html',
+  styleUrls: ['./rfs.component.css']
+})
+export class RfsComponent  {
+
+EmployeeName = new FormControl('');
+
+updateEmployeeName(){
+  this.EmployeeName.setValue('John');
+}
+
+employeeForm = new FormGroup({
+    Name:new FormControl(''),
+    Designation: new FormControl(''),
+  });
+
+  
+}
